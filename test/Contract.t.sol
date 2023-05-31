9fc3f0acfd37e79ebbbbde9d3cdbb3a9e7506fab
QUAQ <56312047+0x0aa0@users.noreply.github.com>
2023-05-31 13:42:08 -0500

    Update Contract.t.sol

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
+pragma solidity 0.8.20;
 
 import "forge-std/Test.sol";
-
 import "src/Contract.sol";
 
 contract TestContract is Test {
@@ -12,12 +11,7 @@ contract TestContract is Test {
         c = new Contract();
     }
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
-    }
-
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
+    function testTest() public {
+        assertEq(uint256(1), uint256(1));
     }
 }
