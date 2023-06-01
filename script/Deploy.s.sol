ec72d9e1389bd780f17b2c45cc880b56f732b939
QUAQ <56312047+0x0aa0@users.noreply.github.com>
2023-06-01 15:03:51 -0500

    Update Deploy.s.sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,12 +2,10 @@
 pragma solidity 0.8.20;
 
 import {Script} from "forge-std/Script.sol";
-import {Contract} from "src/Contract.sol";
 
 contract Deploy is Script {
-    function run() external returns (Contract c) {
+    function run() external {
         vm.startBroadcast();
-        c = new Contract();
         vm.stopBroadcast();
     }
 }
