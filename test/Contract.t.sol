f57edd27e6c63061dee829bfe0b56ed1f15ee6f2
QUAQ <56312047+0x0aa0@users.noreply.github.com>
2023-06-01 15:04:23 -0500

    Update Contract.t.sol

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,16 +2,13 @@
 pragma solidity 0.8.20;
 
 import "forge-std/Test.sol";
-import "src/Contract.sol";
 
 contract TestContract is Test {
-    Contract c;
 
     function setUp() public {
-        c = new Contract();
     }
 
     function testTest() public {
-        assertEq(uint256(1), uint256(1));
     }
+    
 }
